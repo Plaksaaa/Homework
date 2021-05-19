b2040f7a5d7ef2e47bc0910cb4785ce45f12b905
кап <>
2021-05-19 18:47:14 +0300

    New homework

---
+++ src/Homework/Main.java
@@ -0,0 +1,16 @@
+package Homework;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static String isSeven(String a){
+        if (a.endsWith("7")){
+            System.out.println("Строка с 7");
+        }
+        else {
+            System.out.println("Не та строка");
+        }
+        return a;
+    }
+}
